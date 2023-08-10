6454c16c5b930dd01b5181ad0a246a73e51cf1ff
thebesttv <taojuntian@outlook.com>
2023-08-10 22:05:17 +0800

    Split keyboard & SPI finally works

--- keyboards/thebesttv/test/config.h
+++ keyboards/thebesttv/test/config.h
@@ -3,6 +3,18 @@
 
 #pragma once
 
+#define MATRIX_ROWS 10
+#define MATRIX_ROWS_PER_SIDE (MATRIX_ROWS / 2)
+#define MATRIX_COLS 6
+
+#define MATRIX_ONBOARD_ROW_PINS { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+#define MATRIX_ONBOARD_COL_PINS { 0, 0, 0, 0, 0, 0 }
+
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+
+#define MASTER_LEFT // usb cable always connects to the left
+
 #define SERIAL_USART_DRIVER SIOD0
 #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
 #define SERIAL_USART_TX_PIN GP0     // USART TX pin
--- keyboards/thebesttv/test/matrix.c
+++ keyboards/thebesttv/test/matrix.c
@@ -8,45 +8,52 @@
 #    error "not a split keyboard"
 #endif
 
-#ifndef MATRIX_INPUT_PRESSED_STATE
-#    define MATRIX_INPUT_PRESSED_STATE 0
-#endif
-
 void matrix_init_custom(void) {
-    // pin_t slavePin = GP5;
-    // bool lsbFirst = true;
-    // uint8_t mode = 1;
-    // uint16_t divisor = 10;
+    pin_t slavePin = GP5;
+    bool lsbFirst = false;
+    uint8_t mode = 1;
+    uint16_t divisor = 10;
     spi_init();
-    // spi_start(slavePin, lsbFirst, mode, divisor);
+    spi_start(slavePin, lsbFirst, mode, divisor);
 }
 
-void readLocalKeyState(int n, uint8_t layout_map[][2], matrix_row_t result_matrix[]) {
-    // writePinLow(PL);
-    // wait_us(DELAY);
+#define PL GP5
+#define CE GP6
+#define CLK GP2
+#define MOUT GP4
 
-    // writePinHigh(PL);
-    // wait_us(DELAY);
-
-    // writePinLow(CE);
-
-    // for (int i = 0; i < n; i++) {
-    //     writePinHigh(CLK);
-    //     wait_us(DELAY);
-
-    //     if (readPin(MOUT) == PAL_LOW) {
-    //         uint8_t row = layout_map[i][0];
-    //         uint8_t col = layout_map[i][1];
-    //         result_matrix[row] |= MATRIX_ROW_SHIFTER << col;
-    //     }
-
-    //     writePinLow(CLK);
-    //     wait_us(DELAY);
-    // }
-    // writePinHigh(CE);
-}
+#define DELAY 1
 
 #define NUM 31
+#define NUM_8 4
+
+void readLocalKeyState(uint8_t layout_map[][2], matrix_row_t result_matrix[]) {
+    writePinLow(PL);
+    wait_us(DELAY);
+
+    writePinHigh(PL);
+    wait_us(DELAY);
+
+    writePinLow(CE);
+
+    static uint8_t data[NUM_8];
+    spi_receive(data, 4);
+
+    uint8_t *p = data;
+    for (int i = 0; i < NUM;) {
+        uint8_t x = *p++;
+        for (int j = 0; j < 8 && i < NUM; i++, j++) {
+            uint8_t currentBit = x & (1 << (7 - j));
+            if (currentBit == 0) {
+                uint8_t row = layout_map[i][0];
+                uint8_t col = layout_map[i][1];
+                result_matrix[row] |= MATRIX_ROW_SHIFTER << col;
+            }
+        }
+    }
+
+    writePinHigh(CE);
+}
 
 uint8_t LAYOUT_MAP_L[NUM][2] = {
     { 4,  3}, { 4,  4}, { 4,  5},           //  0  1  2 (thumb)
@@ -71,16 +78,12 @@ uint8_t LAYOUT_MAP_R[NUM][2] = {
 };
 
 bool matrix_scan_custom(matrix_row_t raw_matrix[]) {
-    // matrix_row_t current_matrix[MATRIX_ROWS] = {0};
+    matrix_row_t current_matrix[MATRIX_ROWS] = {0};
 
-    // readLocalKeyState(NUM, isLeftHand ? LAYOUT_MAP_L : LAYOUT_MAP_R, current_matrix);
+    readLocalKeyState(isLeftHand ? LAYOUT_MAP_L : LAYOUT_MAP_R, current_matrix);
 
-    // bool changed = memcmp(raw_matrix, current_matrix, sizeof(current_matrix)) != 0;
-    // if (changed) memcpy(raw_matrix, current_matrix, sizeof(current_matrix));
+    bool changed = memcmp(raw_matrix, current_matrix, sizeof(current_matrix)) != 0;
+    if (changed) memcpy(raw_matrix, current_matrix, sizeof(current_matrix));
 
-    // if (changed)
-    //     println("!matrix changed");
-
-    // return changed;
-    return false;
+    return changed;
 }
